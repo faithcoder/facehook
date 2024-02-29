61aa39e701196298ce0cb4656d67ae7c62ef2b35
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-03-01 00:05:48 +0600

    login page updated

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,67 +1,74 @@
+import AuthIllustration from "./assets/images/auth_illustration.png";
+
 export default function LoginPage() {
   return (
-    <div class="max-w-[1368px] flex-1">
-        <div class="container grid items-center gap-8 lg:grid-cols-2">
-          <!-- illustration and title -->
+    <>
+      <div className="max-w-[1368px] flex-1">
+        <div className="container grid items-center gap-8 lg:grid-cols-2">
           <div>
             <img
-              class="mb-12 max-w-full max-lg:hidden"
-              src="./assets/images/auth_illustration.png"
+              className="mb-12 max-w-full max-lg:hidden"
+              src={AuthIllustration}
               alt="auth_illustration"
             />
             <div>
-              <h1 class="mb-3 text-4xl font-bold lg:text-[40px]">Facehook</h1>
-              <p class="max-w-[452px] text-gray-600/95 lg:text-lg">
+              <h1 className="mb-3 text-4xl font-bold lg:text-[40px]">
+                Facehook
+              </h1>
+              <p className="max-w-[452px] text-gray-600/95 lg:text-lg">
                 Create a social media app with features like, showing the post,
                 post details, reactions, comments and profile.
               </p>
             </div>
           </div>
-          <!-- illustration and title ends -->
-          <!-- login form -->
-          <div class="card">
-            <form class="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]">
-              <!-- email -->
-              <div class="form-control">
-                <label class="auth-label" for="email">Email</label>
+
+          <div className="card">
+            <form className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]">
+              <div className="form-control">
+                <label className="auth-label" for="email">
+                  Email
+                </label>
                 <input
-                  class="auth-input"
+                  className="auth-input"
                   name="email"
                   type="email"
                   id="email"
                 />
               </div>
-              <!-- password -->
-              <div class="form-control">
-                <label class="auth-label" for="email">Password</label>
+
+              <div className="form-control">
+                <label className="auth-label" for="email">
+                  Password
+                </label>
                 <input
-                  class="auth-input"
+                  className="auth-input"
                   name="password"
                   type="password"
                   id="password"
                 />
               </div>
-              <!-- Submit -->
+
               <button
-                class="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+                className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
                 type="submit"
               >
                 Login
               </button>
             </form>
-            <div class="py-4 lg:py-6">
-              <p class="text-center text-xs text-gray-600/95 lg:text-sm">
-                Don’t have account?
+            <div className="py-4 lg:py-6">
+              <p className="text-center text-xs text-gray-600/95 lg:text-sm">
+                Dont have account?
                 <a
-                  class="text-white transition-all hover:text-lwsGreen hover:underline"
+                  className="text-white transition-all hover:text-lwsGreen hover:underline"
                   href="/registration.html"
-                  >Create New</a
                 >
+                  Create New
+                </a>
               </p>
             </div>
           </div>
-          <!-- login form ends -->
         </div>
       </div>
-  )
+    </>
+  );
 }
