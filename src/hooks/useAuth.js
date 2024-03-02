33bf409d8631b8cbeca686b7d92cf1d1fd91debe
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-03-02 23:53:44 +0600

    auth provider added

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,10 @@
+import { useContext, useDebugValue } from "react";
+import { AuthContext } from "./AuthContext";
+
+export const useAuth = () => {
+  const { auth } = useContext(AuthContext);
+  useDebugValue(auth, (auth) =>
+    auth?.user ? "User Logged In" : "User Logged Out"
+  );
+  return useContext(AuthContext);
+};
