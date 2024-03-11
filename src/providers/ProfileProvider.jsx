62c3541e2e019b46c084926630a746abaa43fb95
Abdullah Al Arif <140299847+arif-xs@users.noreply.github.com>
2024-03-11 23:54:23 +0600

    profile provider added

---
+++ src/providers/ProfileProvider.jsx
@@ -0,0 +1,17 @@
+import { useReducer } from "react";
+import { ProfileContext } from "../context";
+import { initialState, profileReducer } from "../reducers/ProfileReducers";
+
+const profileProvider = ({ children }) => {{
+  const [state, dispatch] = useReducer(profileReducer, initialState);
+
+  return (
+    <ProfileContext.Provider
+    value={{ state, dispatch }}
+  >
+    {children}
+  </ProfileContext.Provider>;
+  )
+};
+
+export default profileProvider;
